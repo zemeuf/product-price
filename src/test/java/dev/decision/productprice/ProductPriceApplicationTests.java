90f197e2664d257aab96cfdac1290057596e7cc9
cardosojc <jcardoso@kevel.com>
2023-06-14 23:54:07 +0100

    Add a few tests

--- src/test/java/dev/decision/productprice/ProductPriceApplicationTests.java
+++ src/test/java/dev/decision/productprice/ProductPriceApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductPriceApplicationTests {
@@ -18,8 +19,18 @@ class ProductPriceApplicationTests {
 	public void setup() {
 		RestAssured.port = port;
 	}
+
+	@Test
+	void givenNotExistingProduct_whenSearching_thenStatus404() {
+		when().get("/products/48b0a517-ebee-4e75-a514-ccba8f4af4ba/final-price?amount=10").then().statusCode(404);
+	}
 	@Test
 	void givenExistingProduct_whenSearching_thenStatusOK() {
 		when().get("/products/68b0a517-ebee-4e75-a514-ccba8f4af4ba/final-price?amount=10").then().statusCode(200);
+	}
+
+	@Test
+	void givenExistingProduct_whenSearching_thenMatchFinalPrice() {
+		when().get("/products/68b0a517-ebee-4e75-a514-ccba8f4af4ba/final-price?amount=10").then().body("finalPrice", is(2990.0F));
 	}
 }
