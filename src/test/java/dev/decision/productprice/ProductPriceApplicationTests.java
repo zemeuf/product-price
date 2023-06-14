e6eb237d70087fb2824bbe36272f99e8253319b4
cardosojc <jcardoso@kevel.com>
2023-06-14 23:29:01 +0100

    Add first test

--- src/test/java/dev/decision/productprice/ProductPriceApplicationTests.java
+++ src/test/java/dev/decision/productprice/ProductPriceApplicationTests.java
@@ -1,13 +1,25 @@
 package dev.decision.productprice;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static io.restassured.RestAssured.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductPriceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@LocalServerPort
+	private Integer port;
 
+	@BeforeEach
+	public void setup() {
+		RestAssured.port = port;
+	}
+	@Test
+	void givenExistingProduct_whenSearching_thenStatusOK() {
+		when().get("/products/68b0a517-ebee-4e75-a514-ccba8f4af4ba/final-price?amount=10").then().statusCode(200);
+	}
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+DELETE FROM product;
+
+INSERT INTO product (id, product_id, price, discount_type) VALUES (1, '68b0a517-ebee-4e75-a514-ccba8f4af4ba', 10, 0);
+INSERT INTO product (id, product_id, price, discount_type) VALUES (2, '3c56bd40-9c2d-4c96-b130-d836d29bcd52', 25, 1);
